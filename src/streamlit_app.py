8d435488b2ab3c069e7d1323ce52191726462a9d
doremigang81-hash <doremigang81@gmail.com>
2025-12-28 10:58:33 +0700

    Refactor imports and update app folder path
    Refactor file paths and import statements for clarity.

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -1,18 +1,17 @@
-import streamlit as st
-import pickle
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing.sequence import pad_sequences
 import os
 
-# naik 1 level ke root, baru masuk app_sentimen
-APP_FOLDER = os.path.join("..", "app_sentimen")  
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # path ke src/
+APP_FOLDER = os.path.join(BASE_DIR, "..", "app_sentimen")  # naik ke root
 model_path = os.path.join(APP_FOLDER, "bilstm_model.keras")
 tokenizer_path = os.path.join(APP_FOLDER, "tokenizer.pkl")
 
-# Load model & tokenizer
+from tensorflow.keras.models import load_model
+import pickle
+
 model = load_model(model_path)
 with open(tokenizer_path, "rb") as f:
     tokenizer = pickle.load(f)
+
 
 max_len = 100
 
