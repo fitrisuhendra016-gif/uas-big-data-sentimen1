f6b6ea89c228a0ec390f8df59488a1133a640ef5
doremigang81-hash <doremigang81@gmail.com>
2025-12-29 09:59:40 +0700

    Refactor streamlit_app.py for clarity and efficiency
    Removed unused imports and cleaned up code structure.

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -1,27 +1,17 @@
+from tensorflow.keras.models import load_model
+import pickle
 import os
+import streamlit as st
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # path ke src/
-APP_FOLDER = os.path.join(BASE_DIR, "..", "app_sentimen")  # naik ke root
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+APP_FOLDER = os.path.join(BASE_DIR, "..", "app_sentimen")
+
 model_path = os.path.join(APP_FOLDER, "bilstm_model.keras")
 tokenizer_path = os.path.join(APP_FOLDER, "tokenizer.pkl")
 
-from tensorflow.keras.models import load_model
-import pickle
+# ⬇️ INI YANG NAMANYA LOAD MODEL
+model = load_model(model_path, compile=False)
 
-model = load_model(model_path)
 with open(tokenizer_path, "rb") as f:
     tokenizer = pickle.load(f)
-
-
-max_len = 100
-
-st.title("Prediksi Sentimen WhatsApp")
-
-text = st.text_area("Masukkan teks")
-
-if st.button("Prediksi"):
-    seq = tokenizer.texts_to_sequences([text])
-    padded = pad_sequences(seq, maxlen=max_len, padding="post", truncating="post")
-    pred = model.predict(padded)[0][0]
-    label = "Positif 😊" if pred >= 0.5 else "Negatif 😞"
-    st.success(f"Hasil: {label}")
